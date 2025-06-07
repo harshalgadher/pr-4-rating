db7f2c254b7d3e44ce0de7ed55c62fcec81141d0
HARSHAL Gadher <harshalgadher04@gmail.com>
2025-06-07 10:33:43 +0530

    add-selactor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-LSimport React from 'react'
+import React from 'react'
 
 export const Card = () => {
   return (
@@ -20,9 +20,20 @@ export const Card = () => {
               <input type="date" name="password" id="date" class="bg-gray-50 border-2 border-red-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 focus:border-green-500 outline-none " required />
             </div>
           </div>
-          <div>
             <h3 className="text-center text-2xl text-red-500 font-semibold">---Rating---</h3>
-          </div>
+            <div className="text-center">
+              <select name="star" id="star" >
+                  <option value="">---Rate---</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+            </select>
+            </div>
+            <div>
+              <textarea name="comment" rows={5} id="comment" placeholder="Give Your Feed Back" className="w-full border-2 border-red-500 rounded-lg text-lg  focus:border-green-500 outline-none p-2"></textarea>
+            </div>
           <button type="submit" className="w-full text-white bg-red-600 hover:bg-green-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Login to your account</button>
 
         </form>
